6447d37ee7a93b412ab08a74b0c0436231a8eb09
brian <briankin86@gmail.com>
2020-12-04 19:06:53 +0300

    my first attempt

---
+++ task3.3/task3.3/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace task3._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.rbtAdd = new System.Windows.Forms.RadioButton();
+            this.rbtMult = new System.Windows.Forms.RadioButton();
+            this.rbtSub = new System.Windows.Forms.RadioButton();
+            this.rbtDiv = new System.Windows.Forms.RadioButton();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(173, 152);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // rbtAdd
+            // 
+            this.rbtAdd.AutoSize = true;
+            this.rbtAdd.Location = new System.Drawing.Point(299, 22);
+            this.rbtAdd.Name = "rbtAdd";
+            this.rbtAdd.Size = new System.Drawing.Size(44, 17);
+            this.rbtAdd.TabIndex = 1;
+            this.rbtAdd.TabStop = true;
+            this.rbtAdd.Text = "Add";
+            this.rbtAdd.UseVisualStyleBackColor = true;
+            // 
+            // rbtMult
+            // 
+            this.rbtMult.AutoSize = true;
+            this.rbtMult.Location = new System.Drawing.Point(299, 55);
+            this.rbtMult.Name = "rbtMult";
+            this.rbtMult.Size = new System.Drawing.Size(60, 17);
+            this.rbtMult.TabIndex = 2;
+            this.rbtMult.TabStop = true;
+            this.rbtMult.Text = "Multiply";
+            this.rbtMult.UseVisualStyleBackColor = true;
+            // 
+            // rbtSub
+            // 
+            this.rbtSub.AutoSize = true;
+            this.rbtSub.Location = new System.Drawing.Point(299, 89);
+            this.rbtSub.Name = "rbtSub";
+            this.rbtSub.Size = new System.Drawing.Size(65, 17);
+            this.rbtSub.TabIndex = 3;
+            this.rbtSub.TabStop = true;
+            this.rbtSub.Text = "Subtract";
+            this.rbtSub.UseVisualStyleBackColor = true;
+            // 
+            // rbtDiv
+            // 
+            this.rbtDiv.AutoSize = true;
+            this.rbtDiv.Location = new System.Drawing.Point(299, 123);
+            this.rbtDiv.Name = "rbtDiv";
+            this.rbtDiv.Size = new System.Drawing.Size(55, 17);
+            this.rbtDiv.TabIndex = 4;
+            this.rbtDiv.TabStop = true;
+            this.rbtDiv.Text = "Divide";
+            this.rbtDiv.UseVisualStyleBackColor = true;
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(126, 65);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 5;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(126, 106);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.rbtDiv);
+            this.Controls.Add(this.rbtSub);
+            this.Controls.Add(this.rbtMult);
+            this.Controls.Add(this.rbtAdd);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.RadioButton rbtAdd;
+        private System.Windows.Forms.RadioButton rbtMult;
+        private System.Windows.Forms.RadioButton rbtSub;
+        private System.Windows.Forms.RadioButton rbtDiv;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+    }
+}
+
---
+++ task3.3/task3.3/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task3._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool calc_was_clicked = false;
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int output = 0;
+            int num1, num2;
+            
+            if (txtNum1.Text == "" || txtNum2.Text == "")
+            {
+                MessageBox.Show("please enter a number");
+                return;
+            }
+           
+            
+            
+
+            
+            num1 = Int32.Parse(txtNum1.Text);
+            num2 = Int32.Parse(txtNum2.Text);
+            if (rbtAdd.Checked == true)
+            {
+                output = num1 + num2;
+                if (calc_was_clicked)
+                {
+                    MessageBox.Show("ufhh");
+                    return;
+                }
+                else calc_was_clicked = true;
+                MessageBox.Show(" The answer is: " + output);
+
+            }
+            else if (rbtDiv.Checked == true)
+            {
+                output = num1 / num2;
+            }
+            else if (rbtMult.Checked == true)
+            {
+                output = num1 * num2;
+            }
+            else if (rbtSub.Checked == true)
+            {
+                output = num1 - num2;
+            }
+            MessageBox.Show(" The answer is: " + output);
+            
+        }
+    }
+}
